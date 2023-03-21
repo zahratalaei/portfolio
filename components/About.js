d351319c16efbb406ec1f00bc76a533643a32a0a
zahratalaei <zahratalaei@users.noreply.github.com>
2023-03-21 11:48:19 +1000

    modified

---
+++ components/About.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/legacy/image'
+import {BsFillMoonStarsFill} from 'react-icons/bs'
+import { AiFillLinkedin} from 'react-icons/ai'
+import devZ from '../public/zavator.jpg'
+const About = () => {
+  return (
+    
+     <section className="pb-20" id='about '>
+          <div className='flex flex-col items-center md:flex-row md:items-center md:justify-center'>
+              <div className='basis-1/4'>
+                    <div className="relative rounded-full w-80 h-80 mt-20 overflow-hidden md:h-65 md:w-65 md:shrink-0" >
+                    <Image src={devZ} layout='fill' objectFit='cover' className=' ml-40 mr-40' />
+                    </div>
+               </div>
+               <div className='text-left ml-20 basis-1/2'>
+                    <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-5xl dark:text-teal-400'>Zahra Talaei</h2>
+                    <h3 className='text-2xl py-2 md:text-2xl dark:text-white'>Web Developer</h3>
+                    <p className='text-md py-5 text-gray-800 leading-8 md:text-xl md:px-2 max-w-3xl mx-auto dark:text-gray-200 tracking-wide '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, eos animi blanditiis reiciendis esse cum aut recusandae laudantium rerum enim, sed tenetur asperiores totam at illo aliquam error, quibusdam accusantium vitae beatae dolor repudiandae aliquid vero non! Iusto, sint asperiores.
+                    </p>
+               </div>
+          </div>
+          
+     </section>
+
+  )
+}
+
+export default About
---
+++ components/Skills.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <section>
+     <h2 className='text-5xl text-gray-800 py-1 dark:text-white font-bold'>Skills</h2>
+     <ul className='flex flex-wrap gap-1 m-6'>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>JavaScript</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>React Router Dom</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>ReactJs</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>Rest API</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>GraphQL API</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>Bootstrap</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>MUI</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>Tailwind</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>Typescript</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>NextJs</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>NodeJs</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>ExpressJs</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>Styled Components</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>MySQL</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>PostgreSQL</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>sequelize</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>PHP</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>CodeIgniter</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>Symfony</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>WordPress</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>Strapi</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>Webflow</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>Adalo</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>Docker</li>
+          <li className='bg-teal-400 m-1 rounded p-1 text-lg dark:text-white'>Github</li>
+
+     </ul>
+
+    </section>
+  )
+}
+
+export default Skills
