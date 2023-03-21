dbd61eb45480e7e76bd0bd8414e002bc3d9f9ffa
zahratalaei <zahratalaei@users.noreply.github.com>
2023-03-22 09:47:15 +1000

    added shopping cart app

--- components/Projects.js
+++ components/Projects.js
@@ -4,6 +4,7 @@ import lfl from '../public/lfl.png'
 import jumbo from '../public/Jumbo.png'
 import Rtmdb from '../public/ReactTMDB.png'
 import FS from '../public/FullStack.png'
+import Shopping from '../public/Shopping.png'
 import Image from 'next/legacy/image'
 
 const Projects = () => {
@@ -41,6 +42,15 @@ const Projects = () => {
         </a>
         <p className='py-2'>It is a Full Stack Blog App. Being able to see, add comments and likes the posts you need to have an account and sing in to the app. Only author of the post can edit/delete it. </p>
         <p className='text-gray-800 py-1 mt-4'><span className='text-teal-600 py-4 font-bold'>Used Technologies: </span>React, JavaScript, React router dom, react-bootstrap, yup, bootstrap, axios, Mui, ExpressJs, bcrypt, JWT, mysql, sequelize, ... </p>
+      </div>
+      
+      <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
+        <a href='https://main--shappingcart-ts-app.netlify.app/' target="_blank">
+        <Image src={Shopping} alt=''/>
+        <h3 className='text-2xl font-semibold pt-8 pb-2'>Shopping cart app!</h3>
+        </a>
+        <p className='py-2'>It is a shopping cart App which is created by react-typescript. Home page displays different categories. Each category is linked to its products page. This app includes a cart displaying all the selected products with the single price, total price of each item and the total price of cart. </p>
+        <p className='text-gray-800 py-1 mt-4'><span className='text-teal-600 py-4 font-bold'>Used Technologies: </span>React, typescript, React router dom, react-bootstrap, bootstrap5, axios </p>
       </div>
     </div>
  </section>
