38f2dd23edafc9db2fe24a4bf6c53f1b7dad3ba3
zahratalaei <zahratalaei@users.noreply.github.com>
2023-03-21 13:00:41 +1000

    ..

--- components/Projects.js
+++ components/Projects.js
@@ -23,7 +23,7 @@ const Projects = () => {
               <Image src={Rtmdb} alt=''/>
               <h3 className='text-2xl font-semibold pt-8 pb-2'>Search Movies App</h3>
             </a>
-        <p className='py-2'>Through this application you can find the latest popular movies in home page and search the movie by it's name among all the movies in TMDB API. By click on each movie you have access to the details of it. You also be able to paginate through the different pages. All the data comes from TMDB API.</p>
+        <p className='py-2'>Through this application you can find the latest popular movies in home page and search the movie by its name among all the movies in TMDB API. By click on each movie you have access to the details of it. You also be able to paginate through the different pages. All the data comes from TMDB API.</p>
         <p className='text-gray-800 py-1 mt-4'><span className='text-teal-600 py-4 font-bold'>Used Technologies: </span>React, JavaScript, React router dom,Rest API, react-paginate, Styled components</p>
       </div>
       <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
@@ -31,7 +31,7 @@ const Projects = () => {
         <Image src={yhl} alt=''/>
         <h3 className='text-2xl font-semibold pt-8 pb-2'>Your Hospice Lottery Blog</h3>
         </a>
-        <p className='py-2'>This the blog of Your Hospice Lottery website. It's a Wordpress custom theme created from the scratch.</p>
+        <p className='py-2'>This the blog of Your Hospice Lottery website. It is a Wordpress custom theme created from the scratch.</p>
         <p className='text-gray-800 py-1 mt-4'><span className='text-teal-600 py-4 font-bold'>Used Technologies: </span>Wordpress, PHP, bootstrap</p>
       </div>
       <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
@@ -39,7 +39,7 @@ const Projects = () => {
         <Image src={FS} alt=''/>
         <h3 className='text-2xl font-semibold pt-8 pb-2'>Codes Up!</h3>
         </a>
-        <p className='py-2'>It is a Full Stack Blog App. Being able to see, add comments and likes the posts you need to have an account and sing in to the app. Only post's author can edit/delete the post. </p>
+        <p className='py-2'>It is a Full Stack Blog App. Being able to see, add comments and likes the posts you need to have an account and sing in to the app. Only author of the post can edit/delete it. </p>
         <p className='text-gray-800 py-1 mt-4'><span className='text-teal-600 py-4 font-bold'>Used Technologies: </span>React, JavaScript, React router dom, react-bootstrap, yup, bootstrap, axios, Mui, ExpressJs, bcrypt, JWT, mysql, sequelize, ... </p>
       </div>
     </div>
