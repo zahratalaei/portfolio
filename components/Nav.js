2f8e5b4807e945128c92ebf1e81677c908ada15a
zahratalaei <zahratalaei@users.noreply.github.com>
2023-04-20 09:35:35 +1000

    fixed responsibility

--- components/Nav.js
+++ components/Nav.js
@@ -7,11 +7,12 @@ import { Tooltip } from 'flowbite-react'
 const Nav = ({setDarkMode,darkMode}) => {
   return (
     <div>
-      <nav className="py-10 mb-2 flex justify-between dark:text-white">
+     
+      <nav className="py-10 mb-2 flex justify-between dark:text-white sticky">
         <h1 className='text-2xl font-burtons'>Zahra Talaei</h1>
         <ul className='flex items-center'>
          
-          {/* <li><CgDarkMode onClick={()=>setDarkMode(!darkMode)} className='cursor-pointer text-4xl' title={darkMode?"Light Mode":"Dark Mode"}/></li> */}
+       
           
           <li><a href="https://drive.google.com/file/d/1ehbxrfU1B-BBgsaTQdvi6VepMoV6KE81/view?usp=sharing" target="_blank" className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8'>Resume</a></li>
           
@@ -21,13 +22,15 @@ const Nav = ({setDarkMode,darkMode}) => {
           <li className='text-5xl flex justify-center gap-16 py-3 text-teal-600 '>
             <a href="https://github.com/zahratalaei"><DiGithubFull/></a>
           </li>
-          <li className='m-2'>
+          {/* <li><CgDarkMode onClick={()=>setDarkMode(!darkMode)} className='cursor-pointer text-4xl' title={darkMode?"Light Mode":"Dark Mode"}/></li> */}
+          <li className='ml-2'>
             <Tooltip content={darkMode?"Light Mode":"Dark Mode"}>
               <CgDarkMode onClick={()=>setDarkMode(!darkMode)} className='cursor-pointer text-4xl'/>
             </Tooltip>
           </li>
           </ul>
         </nav>
+        
     </div>
   )
 }
