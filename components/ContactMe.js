5809a48b5950643cbfd4f9e1e10ea08fdea3834d
zahratalaei <zahratalaei@users.noreply.github.com>
2023-04-19 22:37:43 +1000

    changed darkmode icon

--- components/ContactMe.js
+++ components/ContactMe.js
@@ -3,7 +3,7 @@ import {AiFillPhone, AiOutlineMail} from 'react-icons/ai'
 const ContactMe = () => {
   return (
      <>
-          <h2 className='text-3xl text-gray-800 py-1 dark:text-white font-semibold '>Contact Me</h2>             
+          <h2 className='text-3xl text-gray-800 py-1 dark:text-white font-bold '>Contact Me</h2>             
                <ul className='flex items-center'>
                     <li className='text-5xl flex justify-center gap-16 py-3 text-teal-600 '>
                          <a href="tel:0411134883"><AiFillPhone/></a>
--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
+import {CgDarkMode} from 'react-icons/cg'
 import { AiFillLinkedin} from 'react-icons/ai'
 import {DiGithubFull} from 'react-icons/di'
 const Nav = ({setDarkMode,darkMode}) => {
@@ -8,11 +9,11 @@ const Nav = ({setDarkMode,darkMode}) => {
       <nav className="py-10 mb-2 flex justify-between dark:text-white">
         <h1 className='text-2xl font-burtons'>Zahra Talaei</h1>
         <ul className='flex items-center'>
-          <li><BsFillMoonStarsFill onClick={()=>setDarkMode(!darkMode)} className='cursor-pointer text-2xl'/></li>
+          <li><CgDarkMode onClick={()=>setDarkMode(!darkMode)} className='cursor-pointer text-2xl'/></li>
           <li><a href="https://drive.google.com/file/d/1ehbxrfU1B-BBgsaTQdvi6VepMoV6KE81/view?usp=sharing" target="_blank" className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8'>Resume</a></li>
           
           <li className='text-5xl flex justify-center gap-16 py-3 text-teal-600 '>
-            <a href="https://www.linkedin.com/feed/"><AiFillLinkedin/></a>
+            <a href="https://www.linkedin.com/in/zahra-talaei-2582069b/"><AiFillLinkedin/></a>
           </li>
           <li className='text-5xl flex justify-center gap-16 py-3 text-teal-600 '>
             <a href="https://github.com/zahratalaei"><DiGithubFull/></a>
