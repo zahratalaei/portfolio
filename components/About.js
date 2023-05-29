fcff9c3d41fb10181d8f933bceb575c5fbace989
zahratalaei <zahratalaei@users.noreply.github.com>
2023-05-29 10:49:36 +1000

    change about me

--- components/About.js
+++ components/About.js
@@ -26,11 +26,10 @@ const {setSelectedPage} = useContext(PageContext)
                     >
                          <h2 className='text-4xl py-2 text-teal-600 font-medium md:text-4xl dark:text-teal-400'>Zahra Talaei</h2>
                          <h3 className='text-2xl py-2 md:text-2xl dark:text-white'>Web Developer</h3>
-                         <p className='text-md py-5 text-gray-800 leading-8 md:text-xl md:px-2 max-w-3xl  dark:text-gray-200 tracking-wide '>An efficient web developer with four years relevant local experience, strong collaboration skills, a keen eye for 
-                         detail and a quick learner. With a background in theoretical physics (mostly computational Physics) which has made her enjoy 
-                         solving problems and handling challenging tasks. Skilled in both individual and team projects. Having worked in multiple 
-                         environments has benefited her with Strong communication and soft skills and experience. Australian Citizen residing in 
-                         Brisbane.
+                         <p className='text-md py-5 text-gray-800 leading-8 md:text-xl md:px-2 max-w-3xl  dark:text-gray-200 tracking-wide '>Hello! I'm a dedicated web developer with over four years of practical experience in the field, based in Brisbane, Australia. I come from a theoretical and computational physics background, which has instilled in me a deep love for problem-solving and tackling intricate tasks. Whether working solo or collaborating as part of a team, my approach to web development is marked by meticulous attention to detail, robust technical knowledge, and a relentless pursuit of innovative solutions.
+                         </p>
+                         <p className='text-md py-5 text-gray-800 leading-8 md:text-xl md:px-2 max-w-3xl  dark:text-gray-200 tracking-wide font-bold'>
+                         Having honed my communication and soft skills across a variety of environments, I'm always eager to engage in new projects and challenges. Through this portfolio, you'll see a showcase of my past projects that reflect my passion and commitment to crafting impactful web experiences. Let's create something amazing together!
                          </p>
                     </motion.div>
                </div>
